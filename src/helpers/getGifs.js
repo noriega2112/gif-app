88ce743a029c50ed2342be27d30d69fcdf2758b3
Edwin Noriega <3170192@usap.edu>
2021-04-03 01:45:11 -0600

    arreglo de https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
    const API_KEY = 'YOXemyF1o8HAMv9egtf5wqs3mrMynaQL';
    const LIMIT = 10;
-   const URL = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=${LIMIT}&q=${encodeURI(category)}`;
+   const URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=${LIMIT}&q=${encodeURI(category)}`;
 
    const resp = await fetch(URL);
    const { data } = await resp.json();
